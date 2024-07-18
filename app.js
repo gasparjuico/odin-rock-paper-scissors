932c2c04666a47757be3d07e3315eae8da75d470
Gaspar <157286423+gasparjuico@users.noreply.github.com>
2024-07-18 19:58:09 +0800

    Add functions to get user and computer choices

--- app.js
+++ app.js
@@ -6,3 +6,24 @@
 // Function to get computer choice
 // Function to determine winner
 // Main game loop
+
+function getPlayerChoice(){
+  let choice = prompt('Enter rock, paper, or scissors:');
+
+  return choice.toLowerCase();
+}
+
+function getComputerChoice(){
+  let computerChoice = Math.floor(Math.random() * 3);
+
+  if (computerChoice === 0){
+    return 'rock';
+  } else if (computerChoice === 1) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+}
+
+console.log(getPlayerChoice());
+console.log(getComputerChoice());
