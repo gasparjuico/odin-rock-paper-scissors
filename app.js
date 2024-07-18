cd2dc12bbcbe2f1407fb59e9c4d76e58d07c74eb
Gaspar <157286423+gasparjuico@users.noreply.github.com>
2024-07-18 19:03:41 +0800

    Add pseudocode

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello World!')
+// Pseudocode
+// Initialize variables: 
+//   Choices: Rock Paper Scissors
+//   Rounds: 5
+// Function to get player choice
+// Function to get computer choice
+// Function to determine winner
+// Main game loop
