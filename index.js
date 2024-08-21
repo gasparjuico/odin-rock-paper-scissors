82ece64f46555c73b605df54b2c618e4afc054c0
Gaspar <157286423+gasparjuico@users.noreply.github.com>
2024-08-21 14:57:16 +0800

    Add functions to play round when a button is clicked
    Add functions to get computer choice. Add an ebent listener to buttons that gets the player choice and play a round. Add a function that plays a round of rock paper scissors.

---
+++ index.js
@@ -0,0 +1,54 @@
+function getComputerChoice(){
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 0){
+      return 'rock';
+    } else if (computerChoice === 1) {
+      return 'paper';
+    } else {
+      return 'scissors';
+    }
+}
+
+// console.log(getComputerChoice())
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let playerChoice = button.textContent;
+        playerChoice = playerChoice.toLowerCase();
+        // console.log(playerChoice)
+        playRound(playerChoice, getComputerChoice());
+    })
+})
+
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerChoice, computerChoice){
+    const resultsContainer = document.querySelector('.results-container');
+
+    if (
+        (playerChoice === 'rock' && computerChoice == 'scissors') ||
+        (playerChoice === 'paper' && computerChoice == 'rock') ||
+        (playerChoice === 'scissors' && computerChoice == 'paper')
+    ) {
+        resultsContainer.textContent = (`You win! ${playerChoice} beats ${computerChoice}`);
+        playerScore++;
+    } else if (
+        (playerChoice === 'rock' && computerChoice == 'paper') ||
+        (playerChoice === 'paper' && computerChoice == 'scissors') ||
+        (playerChoice === 'scissors' && computerChoice == 'rock')
+    ) {
+        resultsContainer.textContent = (`You lose! ${playerChoice} is beaten by ${computerChoice}`);
+        computerScore++;
+    } else {
+        resultsContainer.textContent = (`It's a tie! player chose ${playerChoice} and computer chose ${computerChoice}`);
+    }
+
+    displayScore();
+}
+
+function displayScore() {
+    const scoreContainer = document.querySelector('.score-container');
+    scoreContainer.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+}
